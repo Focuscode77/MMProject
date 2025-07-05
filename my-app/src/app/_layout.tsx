f4aa2fb0fbe487bd8b7bcab0d225bfdf799e2207
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2025-07-05 17:33:33 -0400

    img fixe

--- my-app/src/app/_layout.tsx
+++ my-app/src/app/_layout.tsx
@@ -4,37 +4,49 @@ import AntDesign from '@expo/vector-icons/AntDesign';
 AntDesign;
 
 export default function Layout() {
-  return <Slot />;
-  <Tabs>
-    <Tabs.Screen
-      name='index'
-      options={{
-        title: 'Home',
-        headerShown: false,
-        tabBarIcon: ({ color }) => (
-          <AntDesign name='home' size={24} color={color} />
-        ),
-      }}
-    />
-    <Tabs.Screen
-      name='profile'
-      options={{
-        title: 'Profile',
-        headerShown: false,
-        tabBarIcon: ({ color, size }) => (
-          <AntDesign name='home' size={24} color={color} />
-        ),
-      }}
-    />
-    <Tabs.Screen
-      name='history'
-      options={{
-        title: 'History',
-        headerShown: false,
-        tabBarIcon: ({ color, size }) => (
-          <AntDesign name='clockcircle' size={24} color={color} />
-        ),
-      }}
-    />
-  </Tabs>;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <AntDesign name='home' size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          title: 'Profile',
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name='home' size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='history'
+        options={{
+          title: 'History',
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name='clockcircle' size={24} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name='workouts'
+        options={{
+          title: 'Workouts',
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name='clockcircle' size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
