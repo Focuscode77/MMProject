c0154a3b967333c740f8f96197a01e16d462710f
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2025-07-05 17:00:00 -0400

    Start Up

---
+++ my-app/src/app/_layout.tsx
@@ -0,0 +1,40 @@
+import '../global.css';
+import { Slot, Stack, Tabs } from 'expo-router';
+import AntDesign from '@expo/vector-icons/AntDesign';
+AntDesign;
+
+export default function Layout() {
+  return <Slot />;
+  <Tabs>
+    <Tabs.Screen
+      name='index'
+      options={{
+        title: 'Home',
+        headerShown: false,
+        tabBarIcon: ({ color }) => (
+          <AntDesign name='home' size={24} color={color} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name='profile'
+      options={{
+        title: 'Profile',
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => (
+          <AntDesign name='home' size={24} color={color} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name='history'
+      options={{
+        title: 'History',
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => (
+          <AntDesign name='clockcircle' size={24} color={color} />
+        ),
+      }}
+    />
+  </Tabs>;
+}
---
+++ my-app/src/app/pro.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export default function Page() {
+  return (
+    <SafeAreaView className='flex flex-1'>
+      <Text className='text-center text-2xl font-bold'>Profile</Text>
+    </SafeAreaView>
+  );
+}
